27ffb87ddd2b954b7d1f2ed8a6f93007e0316e25
Eli <elixfoster@gmail.com>
2022-12-23 13:57:20 -0500

    GGs part5

--- backend/db/migrations/20221212075345-create-review.js
+++ backend/db/migrations/20221212075345-create-review.js
@@ -45,6 +45,6 @@ module.exports = {
   },
   async down(queryInterface, Sequelize) {
     options.tableName = 'Reviews'
-    await queryInterface.dropTable('Reviews');
+    await queryInterface.dropTable(options);
   }
 };
