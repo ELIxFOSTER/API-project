334b72fe12bb3869b41ed7fb32ede2f13b115916
Eli <elixfoster@gmail.com>
2022-12-23 13:52:30 -0500

    GGs part3

--- backend/db/migrations/20221212075345-create-review.js
+++ backend/db/migrations/20221212075345-create-review.js
@@ -22,7 +22,8 @@ module.exports = {
       },
       userId: {
         type: Sequelize.INTEGER,
-        references: { model: 'Users' }
+        references: { model: 'Users' },
+        onDelete: 'CASCADE'
       },
       review: {
         type: Sequelize.STRING,
--- backend/db/migrations/20221212075648-create-booking.js
+++ backend/db/migrations/20221212075648-create-booking.js
@@ -23,7 +23,8 @@ module.exports = {
       },
       userId: {
         type: Sequelize.INTEGER,
-        references: { model: "Users" }
+        references: { model: "Users" },
+        onDelete: 'CASCADE'
       },
       startDate: {
         type: Sequelize.DATEONLY,
