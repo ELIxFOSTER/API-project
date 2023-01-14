d36053da48dddd7ef4645240dcfefaf80a3c2b13
Eli <elixfoster@gmail.com>
2023-01-14 15:08:42 -0500

    maybe

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -17,18 +17,8 @@ const removeUser = () => {
   };
 };
 
-export const signup = (user) => async (dispatch) => {
-    const { username, firstName, lastName, email, password } = user;
-    const response = await csrfFetch("/api/users", {
-      method: "POST",
-      body: JSON.stringify({
-        username,
-        firstName,
-        lastName,
-        email,
-        password,
-      }),
-    });
+export const restoreUser = () => async dispatch => {
+    const response = await csrfFetch('/api/session');
     const data = await response.json();
     dispatch(setUser(data.user));
     return response;
@@ -48,20 +38,32 @@ export const login = (user) => async (dispatch) => {
   return response;
 };
 
-export const logout = () => async (dispatch) => {
-    const response = await csrfFetch('/api/session', {
-      method: 'DELETE',
+export const signup = (user) => async (dispatch) => {
+    const { username, firstName, lastName, email, password } = user;
+    const response = await csrfFetch("/api/users", {
+      method: "POST",
+      body: JSON.stringify({
+        username,
+        firstName,
+        lastName,
+        email,
+        password,
+      }),
     });
-    dispatch(removeUser());
-    return response;
-  };
-
-export const restoreUser = () => async dispatch => {
-    const response = await csrfFetch('/api/session');
     const data = await response.json();
     dispatch(setUser(data.user));
     return response;
   };
+
+
+export const logout = () => async (dispatch) => {
+  const response = await csrfFetch('/api/session', {
+    method: 'DELETE',
+  });
+  dispatch(removeUser());
+  return response;
+};
+
 
 const initialState = { user: null };
 
@@ -80,5 +82,7 @@ const sessionReducer = (state = initialState, action) => {
       return state;
   }
 };
+
+
 
 export default sessionReducer;
