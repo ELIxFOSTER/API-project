54d67fbd2d51e871788ed895dcb4d2977dfd2bfe
Eli <elixfoster@gmail.com>
2023-01-22 03:08:48 -0500

    review stars fix

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -430,7 +430,7 @@ const validateReview = [
     .withMessage("Review test is required"),
   check("stars")
     .exists({ checkFalsy: true })
-    .isInt()
+    .isInt({ min: 1, max: 5 })
     .withMessage("Stars must be an integer from 1 to 5"),
   handleValidationErrors,
 ];
