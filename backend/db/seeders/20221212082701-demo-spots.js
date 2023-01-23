9eb34bd3b2be582dad3f93ba85a0ff5a732cb589
Eli <elixfoster@gmail.com>
2023-01-23 08:33:06 -0500

    actually done

--- backend/db/seeders/20221212082701-demo-spots.js
+++ backend/db/seeders/20221212082701-demo-spots.js
@@ -19,7 +19,7 @@ module.exports = {
         lat: 11.05001,
         lng: 22.05001,
         name: 'The Coolest + Most Beautiful House in Cabo + Most Delish!',
-        description: "Experience excitement, luxury and relaxation in this locations ultimate vacation home! Featuring endless outdoor and indoor activities for all ages, this stunning home truly has it all!",
+        description: "Experience excitement, luxury and relaxation in this locations ultimate vacation home!",
         price: 3124
       },
       {
@@ -31,7 +31,7 @@ module.exports = {
         lat: 33.05001,
         lng: 44.00501,
         name: 'Emerald Escape Vacation Home',
-        description: 'Relax and enjoy the views with family and friends! This newly renovated four bedroom home with private swimming pool is a picturesque escape overlooking Emerald Bay. Access the Emerald Bay Marina, multiple beaches or enjoy the sunrise every morning from the second floor balcony. Spend the day with all the beautiful and amazing features of the Exuma’s',
+        description: 'Relax and enjoy the views with family and friends!',
         price: 1500
       },
       {
@@ -67,7 +67,7 @@ module.exports = {
         lat: 99.00501,
         lng: 11.00101,
         name: "Sombras Del Viento - Beachfront Villa With Cook",
-        description: "Sombras Del Viento is a wonderful beachfront abode nestled between the turquoise coloured waters of Soliman Bay and the emerald green jungle of the Yucatan only 15 minutes away from the ever-popular beach town, Tulum. Our property includes a full time cook at your service (except Sundays), a dedicated Maya Luxe Concierge filled with local knowledge to help you craft your perfect holiday and daily maid service (except Sundays)",
+        description: "Sombras Del Viento is a wonderful beachfront abode nestled between the turquoise coloured waters of Soliman Bay and the emerald green jungle of the Yucatan",
         price: 2530
       },
       {
@@ -79,7 +79,7 @@ module.exports = {
         lat: 43.00601,
         lng: 43.02001,
         name: "NEW Luxury Villa available for 2023!",
-        description: "This new modern build offers a relaxing atmosphere, perfect for your upcoming vacation. The new contemporary floor plan features the ultimate comfort, luxury and modern style. From the moment you arrive, you will wrap yourself in complete luxury. You can expect stunning ocean views in all directions from the 10 foot high windows on each floor. ",
+        description: "This new modern build offers a relaxing atmosphere, perfect for your upcoming vacation.",
         price: 2650
       },
       {
@@ -91,7 +91,7 @@ module.exports = {
         lat: 43.06001,
         lng: 43.0002251,
         name: "Luxury Villa with Private Chef Included ! 8 BR Pri",
-        description: "Cenote del Mar is the most luxurious 8 bedroom beachfront villa in all of the Riviera Maya. It is located on the white sandy beaches of Tankah Bay. With 8,600 sq ft of space, high ceilings, spacious rooms, private pool, and a rooftop terrace which gives a 360-degree view of the entire bay.",
+        description: "Cenote del Mar is the most luxurious 8 bedroom beachfront villa in all of the Riviera Maya.",
         price: 4160
       },
       {
@@ -103,7 +103,7 @@ module.exports = {
         lat: 43.02001,
         lng: 43.00201,
         name: "Luxurious Casa Symbiotica - sleeps 19 guests!",
-        description: "High end resort-like home in mountaintop community with 180 degree views of the Pacific Ocean. 8 minute drive to 5 beautiful beaches. Extremely secure with live-in guard, private gate. Full-time maid service. Modern touches throughout including media room, game room, gym and Sonos sound system. Due to my position as the CEO of a cleaning chemical manufacturing company, we go to extra lengths to disinfect our home with our own EPA registered Covid Killing disinfectants. ",
+        description: "High end resort-like home in mountaintop community with 180 degree views of the Pacific Ocean. ",
         price: 2485
       },
       {
@@ -115,7 +115,7 @@ module.exports = {
         lat: 43.02001,
         lng: 43.00201,
         name: "Villa sleeps 16, Movie theater, Pool, & Jacuzzi !",
-        description: "Our Villa Justina is a one of a kind , combining Tudor architecture with modern elements, a huge mansion sitting in the middle of a jungle of mango and palm trees with a large pool and cascade in one of the most sought after  neighborhoods. Perfect for large groups of friends and big families",
+        description: "Our Villa Justina is a one of a kind , combining Tudor architecture with modern elements.",
         price: 1810
       },
       {
@@ -127,7 +127,7 @@ module.exports = {
         lat: 43.2001,
         lng: 43.00021,
         name: "Lakefront Luxury Villa",
-        description: "Grand and sophisticated villa on peninsula at Lake Martin. Winding staircase, marble floors, two story windows, and stylish details at every turn. Prepare meals in the gourmet kitchen. Make memories in the billiard area, swim and sun on the expansive pool deck. Forget your worries in this spacious and serene space.",
+        description: "Grand and sophisticated villa on peninsula at Lake Martin.",
         price: 1742
       },
       // {
